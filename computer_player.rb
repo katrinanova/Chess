75d230c4ca0edd5a56780736ab80d171c2ad4f5b
Max and Katrina <katrina_max@appacademy.com>
2015-06-03 17:53:14 -0400

    Implementing recursive AI, w.i.p

--- computer_player.rb
+++ computer_player.rb
@@ -18,8 +18,20 @@ class ComputerPlayer < Player
     get_best_move
   end
 
+
+  def get_best_move
+    candidates = get_legal_moves
+    attack_moves = get_attack_moves(candidates)
+    if attack_moves.empty?
+      best_move = recursive_move_search(candidates)
+    else
+      best_move = find_best_attack(attack_moves)
+    end
+
+    best_move
+  end
+
   def get_legal_moves
-    debugger
     pieces = @board.get_color_pieces(@color)
     moves = []
     pieces.each do |piece|
@@ -29,19 +41,18 @@ class ComputerPlayer < Player
     moves
   end
 
-  def get_best_move
-    candidates = get_legal_moves
-    attack_moves = candidates.select do |move|
+  def get_attack_moves(candidates)
+    candidates.select do |move|
       @board.occupied?(move[1]) &&
       @board.enemy?(move[1], color)
     end
-    return candidates.sample if attack_moves.empty?
+  end
 
+  def find_best_attack(candidates)
     best_move = []
     best_value = 0
 
-    debugger
-    attack_moves.each do |move|
+    candidates.each do |move|
       value = PIECE_VALUES[@board[*move[1]].class]
       if value >= best_value
         best_value = value
@@ -50,6 +61,51 @@ class ComputerPlayer < Player
     end
 
     best_move
+  end
+
+  ##DANGEROUS METHODS BELOW
+
+  def recursive_move_search(candidates)
+    # future_move_values = Hash.new {|h,k| h[k] = 0 }
+    candidates.each do |candidate|
+      value = simulate_own_move(candidate)
+      return candidate unless value.nil?
+    end
+
+    # best_value = future_move_values.values.max
+    # future_move_values.invert[best_value]
+  end
+
+  def simulate_own_move(move)
+    new_board = @board.deep_dup
+    new_board.move!(*move)
+    old_board = @board
+    @board = new_board
+    opponent_move = simulate_opponent_moves
+    @board.move!(*opponent_move)
+    best_move = get_best_move
+
+    value = nil
+
+    if @board.occupied?([*best_move[1]])
+      @board = old_board
+      return PIECE_VALUES[@board[*best_move[1]]]
+    end
+
+    @board = old_board
+
+    nil
+  end
+
+  def simulate_opponent_moves
+    switch_color
+    move = get_best_move
+    switch_color
+    move
+  end
+
+  def switch_color
+    color == :black ? :white : :black
   end
 
 end
